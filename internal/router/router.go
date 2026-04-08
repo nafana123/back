af1c1eccfc146199025daeb447f2cef1f56a1480
qweshka <vladoperation@bk.ru>
2026-04-08 13:45:20 +0500

    my saved changes

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,8 +1,12 @@
 package router
 
 import (
-	"back/internal/handler/auth"
+	"back/internal/handler/admin"
+	auth "back/internal/handler/auth"
 	"back/internal/handler/health"
+	"back/internal/repository"
+	"back/internal/service"
+	"gorm.io/gorm"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,7 +14,7 @@ import (
 	"go.uber.org/zap"
 )
 
-func New(log *zap.Logger, authHandler *auth.AuthHandler) http.Handler {
+func New(log *zap.Logger, authHandler *auth.AuthHandler, db *gorm.DB) http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(cors.Handler(cors.Options{
@@ -22,14 +26,36 @@ func New(log *zap.Logger, authHandler *auth.AuthHandler) http.Handler {
 		MaxAge:           300,
 	}))
 
-	r.Get("/api/health", health.HealthHandler)
+	tournamentRepo := repository.NewTournamentRepository(db)
+	gameRepo := repository.NewGameRepository(db)
 
-	r.Route("/api/auth", func(r chi.Router) {
-		r.Post("/telegram", authHandler.TelegramAuth)
-		r.Post("/registration", authHandler.Registration)
-		r.Post("/login", authHandler.Login)
-		r.Get("/steam", authHandler.SteamAuth)
-		r.Get("/steam/callback", authHandler.SteamCallback)
+	tournamentService := service.NewTournamentService(tournamentRepo, gameRepo)
+	gameService := service.NewGameService(tournamentRepo, gameRepo)
+
+	tournamentHandler := admin.NewTournamentHandler(log, tournamentService)
+	gameHandler := admin.NewGameHandler(log, gameService)
+
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/health", health.HealthHandler)
+
+		r.Route("/auth", func(r chi.Router) {
+			r.Post("/telegram", authHandler.TelegramAuth)
+			r.Post("/registration", authHandler.Registration)
+			r.Post("/login", authHandler.Login)
+			r.Get("/steam", authHandler.SteamAuth)
+			r.Get("/steam/callback", authHandler.SteamCallback)
+		})
+
+		r.Route("/admin", func(r chi.Router) {
+			r.Post("/tournaments", tournamentHandler.CreateTournament)
+			r.Patch("/tournaments/{id}/status", tournamentHandler.ChangeTournamentStatus)
+
+			r.Get("/games", gameHandler.GetGames) // todo сделать
+		})
+
+		r.Get("/tournaments", tournamentHandler.GetTournaments)
+		r.Get("/tournaments/{id}/join", tournamentHandler.GetTournament)
+		r.Get("/tournaments/{id}", tournamentHandler.GetTournament)
 	})
 
 	return r
